ae624936206428d2eb0fb8d0c6104334dbd0f8c8
salarmafi <salarmafi95@gmail.com>
2024-05-27 12:46:31 +0330

    Finishing The Project

--- Project_one.py
+++ Project_one.py
@@ -3,9 +3,19 @@
 import random
 print('A number between 1 - 10')
 a_number = round(random.random() * 10)
-user_guess = int(input('Enter a guess between 0 - 10: '))
 
-if user_guess == a_number:
-    print('Congrats!')
-else:
-    print('Sorry!')
+while True:
+
+    user_guess = int(input('Enter a guess between 0 - 10: '))
+    if user_guess == a_number:
+        print('Congrats!')
+        break
+    else:
+        print('Sorry')
+        user_guess = input('Continue? [Y / N] -> ')
+        if user_guess.lower() == 'y':
+            continue
+        else:
+            print('You quitted')
+            break
+

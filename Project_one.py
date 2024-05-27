85c2ebd02ee1896843950b9f2acce39884da6c94
salarmafi <salarmafi95@gmail.com>
2024-05-27 12:34:09 +0330

    Adding if-else Logic to Project One

--- Project_one.py
+++ Project_one.py
@@ -3,4 +3,9 @@
 import random
 print('A number between 1 - 10')
 a_number = round(random.random() * 10)
-print(a_number)
+user_guess = int(input('Enter a guess between 0 - 10: '))
+
+if user_guess == a_number:
+    print('Congrats!')
+else:
+    print('Sorry!')
